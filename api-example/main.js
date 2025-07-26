9593d77d355a351714b1495efdccf030b571fc6a
Geneen <geneen850@gmail.com>
2025-07-26 14:10:15 +0300

    fix request mistake

--- api-example/main.js
+++ api-example/main.js
@@ -2,13 +2,15 @@
   let xhr = new XMLHttpRequest();
   let url =
     "http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC";
-  onreadystatechange = function() {
+  xhr.onreadystatechange = function() {
     if (xhr.readyState == 4 && xhr.status == 200) {
       let giphyObj = xhr.responseText;
+      giphyObj = JSON.parse(giphyObj);
       let gifDrop = document.querySelector(".gif");
       let link = giphyObj.data[1].images.downsized_medium.url;
       gifDrop.src = link;
     }
   };
   xhr.open("GET", url, true);
+  xhr.send();
 })();
